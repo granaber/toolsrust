6456aa650ca8dc965c7db63ea9d9efe9410c9724
Angel Granadi110 <angelegranadillo@gmail.com>
2022-11-02 17:23:36 -0400

    feat Select load with select platform

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const tr = require('./index.node')
+const [DARWIN, GNU, WIN] = ['darwin', 'linux', 'win32']
+const osdata = {
+  [DARWIN]: 'darwin',
+  [GNU]: 'gnu',
+  [WIN]: 'win'
+}
+const os = require('os')
+const osmain = os.platform()
+
+const file_require = './index.' + osdata[osmain] + '.node'
+
+const tr = require(file_require)
 
 exports.random = function (start, end) {
   const i = tr.random(start, end)
